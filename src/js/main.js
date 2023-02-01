66edaa39c14b961a44086aa6191cb04eb54b7350
YukiLee0508 <118801898+YukiLee0508@users.noreply.github.com>
2023-02-01 23:58:09 +0800

    test

--- src/js/main.js
+++ src/js/main.js
@@ -82,20 +82,22 @@ email.addEventListener('keyup', () => {
 //设置一个邮箱输入函数
 function checkEmail(info) {
 
-  const emailMsg = document.getElementById('email_Msg')
+  const emailMsg = document.getElementById("email_Msg")
+  const emailGroup = document.getElementById("email_group")
+
   const emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
-  // const emailGroup = document.getElementById('email_Group')
 
-  console.log(emailReg.test(info));
-
-  if (emailReg.test(info)) {
-
-    emailMsg.textContent = "success Email"
-    emailGroup.classList.remove('success')
-    emailGroup.classList.remove('error')
-  } else {
+  if (!emailReg.test(info)) {
+    console.log("000000000");
+    console.log(emailMsg);
     emailMsg.textContent = "Error Email"
-    emailGroup.classList.remove('error')
     emailGroup.classList.remove('success')
+    emailGroup.classList.add('error')
+  } else {
+    console.log("1111");
+    console.log(emailMsg);
+    emailMsg.textContent = "success Email"
+    emailGroup.classList.remove('error')
+    emailGroup.classList.add('success')
   }
 }
